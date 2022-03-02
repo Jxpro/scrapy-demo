bd52e3e9bbb46a23ba442d0a20cf325f1294c519
jokerxin <jxpro@qq.com>
2022-03-02 22:01:57 +0800

    Update cookies set method test

--- downloadertest/downloadertest/spiders/httpbin.py
+++ downloadertest/downloadertest/spiders/httpbin.py
@@ -6,5 +6,9 @@ class HttpbinSpider(scrapy.Spider):
     allowed_domains = ['httpbin.org']
     start_urls = ['http://httpbin.org/get']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url,cookies={'cookie': 'test:cookies_spider'})
+
     def parse(self, response, **kwargs):
         self.logger.debug(response.text)
